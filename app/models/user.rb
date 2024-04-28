53fc0dbc1a0d3963420196705499b89bd1a6dd3a
Joshua Hawks <jdhawks@gmail.com>
2024-04-28 13:00:14 -0500

    add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  enum role: { user: 0, admin: 1, superadmin: 2 }
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def admin?
+    role == 'admin'
+  end
+
+  def superadmin?
+    role == 'superadmin'
+  end
+
 end
